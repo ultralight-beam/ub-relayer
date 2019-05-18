952db78d101a86ab11e7bcc3c10c9da462be1b2a
Cayman <caymannava@gmail.com>
2019-05-18 11:34:54 -0500

    Update uuid and logging

--- characteristics/whisper.js
+++ characteristics/whisper.js
@@ -4,9 +4,11 @@ var bleno = require('bleno');
 
 var BlenoCharacteristic = bleno.Characteristic;
 
+const UUID = '00000000-0000-0000-0000-000000000001'
+
 var EchoCharacteristic = function() {
   EchoCharacteristic.super_.call(this, {
-    uuid: 'ec0e',
+    uuid: UUID,
     properties: ['read', 'write', 'notify'],
     value: null
   });
@@ -18,7 +20,8 @@ var EchoCharacteristic = function() {
 util.inherits(EchoCharacteristic, BlenoCharacteristic);
 
 EchoCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  console.log('EchoCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
+  console.log('EchoCharacteristic - onReadRequest: value = ' + this._value.toString('utf8'));
+  console.log(this._value);
 
   callback(this.RESULT_SUCCESS, this._value);
 };
@@ -26,7 +29,8 @@ EchoCharacteristic.prototype.onReadRequest = function(offset, callback) {
 EchoCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
   this._value = data;
 
-  console.log('EchoCharacteristic - onWriteRequest: value = ' + this._value.toString('hex'));
+  console.log('EchoCharacteristic - onWriteRequest: value = ' + this._value.toString('utf8'));
+  console.log(this._value);
 
   if (this._updateValueCallback) {
     console.log('EchoCharacteristic - onWriteRequest: notifying');
