5107c53bbd30f6ebd324c6f1ddf5842715f53593
Cayman <caymannava@gmail.com>
2019-05-18 18:36:39 -0500

    Remove web3

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const bleno = require('bleno');
-const Web3 = require( 'web3');
-const Shh = require ('web3-shh').Shh;
 
 const UB_RELAYER_NAME = 'UB Relayer'
 const UB_SSID = '00756c74-7261-6c69-6768-74206265616d'
@@ -10,18 +8,6 @@ let BlenoPrimaryService = bleno.PrimaryService;
 let characteristics = require('./characteristics');
 
 console.log('UB relayer');
-
-const shh = new Shh('ws://0.0.0.0:8546', null, {});
-shh.subscribe('messages', {
-    symKeyID: '',
-    sig: '',
-    ttl: 20,
-    topics: ['9ad157d0'],
-    minPow: 0.8,
-}, (error, message, subscription) => {
-
-    console.log(message);
-})
 
 bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
